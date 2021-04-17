8cd19a725181dfdc3e8a0eea4f02921457125178
Joel <joeld200013@gmail.com>
2021-04-17 08:58:43 -0400

    Add ROC and AUC calculus

--- first-model.py
+++ first-model.py
@@ -1,9 +1,12 @@
 import pandas as pd
+from matplotlib import pyplot
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
 
 dataset = pd.read_csv("cancer-data.csv")
 pd.set_option('display.max_rows', None)
@@ -48,10 +51,36 @@ Y_pred = classifier.predict(X_test)
 cm = confusion_matrix(Y_test, Y_pred)
 correct_predictions = cm[0, 0] + cm[1, 1]
 total_number_predictions = cm[0, 0] + cm[0, 1] + cm[1, 0] + cm[1, 1]
-
 accuracy = correct_predictions / total_number_predictions
+
 print(f'Verdadero positivo: {cm[0,0]}')
 print(f'Falso positivo: {cm[0,1]}')
 print(f'Falso negativo: {cm[1, 0]}')
 print(f'Verdadero negativo: {cm[1, 1]}')
 print(f'Precision del modelo: {accuracy*100} %')
+
+# Generacion de prediccion sin skills
+ns_probs = [0 for _ in range(len(Y_test))]
+
+# Prediccion de probabilidades
+lr_probs = classifier.predict_proba(X_test)
+lr_probs = lr_probs[:, 1]
+
+# Calculo de scores para area debajo de la curva(AUC)
+ns_auc = roc_auc_score(Y_test, ns_probs)
+lr_auc = roc_auc_score(Y_test, lr_probs)
+print('No Skill: ROC AUC=%.3f' % (ns_auc))
+print('Logistic: ROC AUC=%.3f' % (lr_auc))
+
+ns_fpr, ns_tpr, _ = roc_curve(Y_test, ns_probs)
+lr_fpr, lr_tpr, _ = roc_curve(Y_test, lr_probs)
+
+# Grafica de curva
+pyplot.plot(ns_fpr, ns_tpr, linestyle='--', label='No Skill')
+pyplot.plot(lr_fpr, lr_tpr, marker='.', label='Logistic')
+
+pyplot.xlabel('False Positive Rate')
+pyplot.ylabel('True Positive Rate')
+pyplot.legend()
+
+pyplot.show()
